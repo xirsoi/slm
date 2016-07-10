7c37ce3654a758f0424c16b71be403fa18c20d63
xirsoi <chevalierAdam2@gmail.com>
2016-07-10 12:12:35 -0500

    adds ancient religions compatibility sub-mod

--- create_symbolic_links.bat
+++ create_symbolic_links.bat
@@ -1,4 +1,5 @@
 REM Creates a symbolic link between CKII mod folder and working directory of git repository.
 REM Note: .mod file still needs to be copied manually
 mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\slm" ".\slm"
+mklink /J "%HOMEDRIVE%%HOMEPATH%\Documents\Paradox Interactive\Crusader Kings II\mod\slm_ACR_comp" ".\slm_ACR_comp"
 pause
---
+++ slm_AR_comp.mod
@@ -0,0 +1,5 @@
+name = "SLM/Ancient Religions"
+path = "mod/slm_AR_comp"
+picture = "SLM_AR.jpg"
+tags = { Gameplay Decisions War }
+dependencies = { "Ancient Religions Reborn" "Stormy's Little Mod" }
